a58eac288add527801a9c7048c172caba079eb58
apple <123456789@qq.com>
2020-08-28 16:40:24 +0800

    qq音乐

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        <h1>1111</h1>
+        <m-header></m-header>
     </div>
 </template>
 
 <script>
+import MHeader from './components/m-header/m-header'
 export default {
-    
+    // 注册组件
+    components:{
+        MHeader,
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import router from './router/router'
 import store from './store/store'
 import App from './App.vue'
 
+// 解决移动端点击有300ms延迟的问题
+import fastclick from "fastclick"
+fastclick.attach(document.body)
+
+// 引入全局样式
+import "./common/stylus/index"
+
 new Vue({
     router,
     store,
